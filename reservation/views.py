4edfd61532a1f92e625f4afb101d848814d9e963
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-03-18 19:31:29 -0500

    subtle changes, still getting things started

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# from .models import Menu
 
 # Create your views here.
+
+# def menu(request):
+#   menu_data = Menu.objects.all()
+#   main_data = {"menu": menu_data}
+#   return render(request, "menu.html", {"menu": main_data})
+
+# the stuff above is used to show what html is loaded in for each view
